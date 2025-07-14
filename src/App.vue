4de2e4810ca3aa2b32ccdc5f089c44e4ed9615bc
e.kochetova <pandoraizida@gmail.com>
2025-07-14 16:53:25 +0200

    added the first page

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,110 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+//import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
+  <header>
+   <div><img alt="logo" class="logo" src="./components/icons/cat1.jpg"/></div>
+   <div class="greeting">Today's brilliant word for practice:</div>
   </header>
 
   <main>
-    <TheWelcome />
+    <div class="container">
+      <div class="cont cont_word">
+        <div class="wd wd_word">Word from Dictionary</div>
+        <div class="wd wd_type">Word type</div>
+      </div>
+      <div class="cont cont_description">
+        <div class="wd wd_desc">Word description hdsfhjkds fdsjkhfhsd dfsjfhhds dsjhafjkdsa dfshjkhfjkdas dsjfhdsajhfsda fadsjhfhsd</div>
+        <div class="wd wd_example">Example how to use this word Example how to use this word Example how to use this word Example how to use this word. </div>
+      </div>
+    </div>
   </main>
+
+  <footer>
+    Have fun!
+  </footer>
 </template>
 
 <style scoped>
+
 header {
-  line-height: 1.5;
+  height: 10%;
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  padding: 30px;
 }
 
 .logo {
-  display: block;
-  margin: 0 auto 2rem;
+  flex-basis: 30%;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.greeting {
+  font-size: 38px;
+  color: rgb(69, 6, 94);
+  font-weight: 800;
+  text-align: center;
+  flex-basis: 70%;
+  padding-top: 90px;
 }
+
+main {
+  width: 100%;
+  height: 100%;
+}
+
+.container {
+  display: flex;
+  width: 100%;
+  height: 100%;
+  flex-direction: row;
+}
+
+.cont {
+  display: flex;
+  flex-direction: column;
+}
+
+.cont_word {
+  flex-basis: 40%;
+}
+
+.cont_description {
+  flex-basis: 60%;
+}
+
+.wd {
+  text-align: left;
+  font-size: 30px;
+  color: rgb(69, 43, 93);
+  padding: 30px;
+}
+
+.wd_word {
+  padding: 70px 30px 30px 30px;
+  font-weight: bold;
+  font-size: 35pt;
+  color: rgb(38, 2, 2);
+}
+
+.wd_type {
+  font-size: 15px;
+  color: rgb(45, 1, 41);
+}
+
+.wd_desc {
+  padding: 80px 30px 30px 30px;
+}
+
+footer {
+  font-size: 35px;
+  color: rgb(69, 6, 94);
+  font-weight: 800;
+  text-align: center;
+  height: 5%;
+  margin-top: 80px;
+}
+
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -8,17 +8,20 @@ defineProps({
 </script>
 
 <template>
-  <div class="greetings">
+  <div class="main_block">
     <h1 class="green">{{ msg }}</h1>
     <h3>
-      You’ve successfully created a project with
-      <a href="https://vite.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
+      You’ve successfully created a project!!
     </h3>
   </div>
 </template>
 
 <style scoped>
+.main_block {
+  border-style: dotted;
+  border-color: blueviolet;
+}
+
 h1 {
   font-weight: 500;
   font-size: 2.6rem;
@@ -30,15 +33,9 @@ h3 {
   font-size: 1.2rem;
 }
 
-.greetings h1,
+/* .greetings h1,
 .greetings h3 {
   text-align: center;
-}
+} */
 
-@media (min-width: 1024px) {
-  .greetings h1,
-  .greetings h3 {
-    text-align: left;
-  }
-}
 </style>
